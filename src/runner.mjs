024eeead334703318b370af9c0dbf135d6b8f1f5
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 12:20:27 +0200

    Add test results and update results output

--- src/runner.mjs
+++ src/runner.mjs
@@ -4,12 +4,23 @@ import { color } from './colors.mjs'
 
 const config = getConfig()
 
+let anyFailures = false
+let successes = 0
+let failures = 0
+
+const exitCodes = {
+  ok: 0,
+  failures: 1
+}
+
 export const it = (name, body) => {
   try {
     body()
+    successes++
   } catch (e) {
     console.error(color(`<red>${name}</red>`))
     console.error(e)
+    failures++
   }
 }
 
@@ -19,5 +30,10 @@ export const run = async () => {
   } catch (e) {
     console.error(e)
   }
-  console.log('Test run finished')
+  console.log(
+    color(
+      `Tests: <green>${successes} passed</green>, <red>${failures} failed</red>`
+    )
+  )
+  process.exit(failures !== 0 ? exitCodes.failures : exitCodes.ok)
 }
