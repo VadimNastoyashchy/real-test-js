5a25cc0f620d065b1fc0eb47f1e57af3dfde7d2b
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 12:03:27 +0200

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,1 +1,10 @@
-console.log('TEST file')
+import { it } from '../src/runner.mjs'
+
+it('First pass test', () => {
+  console.log('Pass test')
+})
+
+it('First fail test', () => {
+  console.log('Fail test')
+  throw new Error('Error fail test')
+})
