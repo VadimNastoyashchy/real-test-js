962ee4e0bd6bbc7f7d8b8eac020f86655e59160d
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 16:30:40 +0200

    Update core structure

--- src/core.mjs
+++ src/core.mjs
@@ -9,6 +9,40 @@ let successes = 0
 const failures = []
 let describeStack = []
 
+// Runner entry point
+export const run = async () => {
+  try {
+    await import(path.resolve(process.cwd(), config.specFile))
+  } catch (e) {
+    console.error(e)
+  }
+  printFailuresMsg()
+  console.log(
+    applyColor(
+      `Tests: <green>${successes} passed</green>, ` +
+        `<red>${failures.length} failed</red>.`
+    )
+  )
+  process.exit(failures.length > 0 ? EXIT_CODES.failures : EXIT_CODES.ok)
+}
+
+export const describe = (name, body) => {
+  describeStack = [...describeStack, name]
+  body()
+  describeStack = withoutLast(describeStack)
+}
+
+export const it = (name, body) => {
+  try {
+    body()
+    console.log(indent(applyColor(`  <green>${TICK}</green> ${name}`)))
+    successes++
+  } catch (e) {
+    console.error(indent(applyColor(`  <red>${CROSS}</red> ${name}`)))
+    failures.push({ error: e, name, describeStack })
+  }
+}
+
 const printFailureMsg = (failure) => {
   console.error(applyColor(fullTestDescription(failure)))
   console.error(failure.error)
@@ -30,37 +64,3 @@ const fullTestDescription = ({ name, describeStack }) =>
   [...describeStack, name].map((name) => `<bold>${name}</bold>`).join(' → ')
 
 const indent = (message) => `${' '.repeat(describeStack.length * 2)}${message}`
-
-// Runner entry point
-export const run = async () => {
-  try {
-    await import(path.resolve(process.cwd(), config.specFile))
-  } catch (e) {
-    console.error(e)
-  }
-  printFailuresMsg()
-  console.log(
-    applyColor(
-      `Tests: <green>${successes} passed</green>, ` +
-        `<red>${failures.length} failed</red>.`
-    )
-  )
-  process.exit(failures.length > 0 ? EXIT_CODES.failures : EXIT_CODES.ok)
-}
-
-export const it = (name, body) => {
-  try {
-    body()
-    console.log(indent(applyColor(`  <green>${TICK}</green> ${name}`)))
-    successes++
-  } catch (e) {
-    console.error(indent(applyColor(`  <red>${CROSS}</red> ${name}`)))
-    failures.push({ error: e, name, describeStack })
-  }
-}
-
-export const describe = (name, body) => {
-  describeStack = [...describeStack, name]
-  body()
-  describeStack = withoutLast(describeStack)
-}
