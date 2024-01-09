697880135466c946a20e517eb9453135963b4b0f
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 14:30:43 +0200

    Update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: 'standard',
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
-  }
+    'comma-dangle': 'off',
+  },
 }
