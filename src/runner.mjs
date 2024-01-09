9267eddf9fd3c89785709ac54eba2a320d389bac
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 12:03:11 +0200

    Add it block for test

--- src/runner.mjs
+++ src/runner.mjs
@@ -1,7 +1,17 @@
 import path from 'path'
 import { getConfig } from './config.mjs'
+import { color } from './colors.mjs'
 
 const config = getConfig()
+
+export const it = (name, body) => {
+  try {
+    body()
+  } catch (e) {
+    console.error(color(`<red>${name}</red>`))
+    console.error(e)
+  }
+}
 
 export const run = async () => {
   try {
