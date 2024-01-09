28141d96ccb6cc8f9b09a6143deeb97d283f2d01
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 23:20:43 +0200

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
 import { describe, it } from '../src/core.mjs'
 
 it('First it', () => {
-  console.log('Inside first it')
-})
-
-it('Second it', () => {
-  console.log('Inside second it')
-  // throw new Error('Error fail test')
-})
-
-it('Third it', () => {
-  console.log('Inside third it')
+  console.log('1')
 })
 
 describe('First describe', () => {
-  console.log('Inside first describe')
+  console.log('2')
   it('First it inside first describe', () => {
-    console.log('First it inside first describe')
+    console.log('3')
     // throw new Error('Error fail test')
+  })
+})
+
+describe('Second describe', () => {
+  console.log('4')
+  describe('Inside second describe > nested describe', () => {
+    console.log('5')
+    it('First it inside nested describe', () => {
+      console.log('6')
+      // throw new Error('Error fail test')
+    })
   })
 })
