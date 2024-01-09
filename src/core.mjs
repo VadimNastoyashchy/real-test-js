94151fb3f2f9938359f0042dd8716df3968a1e74
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-09 14:51:41 +0200

    Refactor color transformation

--- src/core.mjs
+++ src/core.mjs
@@ -1,41 +1,52 @@
 import path from 'path'
 import { getConfig } from './config.mjs'
-import { color } from './colors.mjs'
-import { TICK, CROSS } from './constants.mjs'
+import { applyColor } from './transform.mjs'
+import { TICK, CROSS, EXIT_CODES } from './constants.mjs'
 
 const config = getConfig()
 
 let successes = 0
-let failures = 0
+const failures = []
 
-const exitCodes = {
-  ok: 0,
-  failures: 1,
+const printFailure = (e) => {
+  console.error(e)
+  console.error('')
 }
 
+const printFailures = () => {
+  if (failures.length > 0) {
+    console.error('')
+    console.error('Failures:')
+    console.error('')
+  }
+  failures.forEach(printFailure)
+}
+
+// Runner entry point
 export const run = async () => {
   try {
     await import(path.resolve(process.cwd(), config.specFile))
   } catch (e) {
     console.error(e)
   }
+  printFailures()
   console.log(
-    color(
-      `Tests: <green>${successes} passed</green>, <red>${failures} failed</red>`
+    applyColor(
+      `<green>${successes}</green> tests passed, ` +
+        `<red>${failures.length}</red> tests failed.`
     )
   )
-  process.exit(failures !== 0 ? exitCodes.failures : exitCodes.ok)
+  process.exit(failures.length > 0 ? EXIT_CODES.failures : EXIT_CODES.ok)
 }
 
 export const it = (name, body) => {
   try {
     body()
-    console.log(color(`  <green>${TICK}</green> ${name}`))
+    console.log(applyColor(`  <green>${TICK}</green> ${name}`))
     successes++
   } catch (e) {
-    console.log(color(`  <red>${CROSS}</red> ${name}`))
-    console.error(e)
-    failures++
+    console.error(applyColor(`  <red>${CROSS}</red> ${name}`))
+    failures.push(e)
   }
 }
 
