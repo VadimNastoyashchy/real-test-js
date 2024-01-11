6594f78ba12071726e3b9e128f24a51a828a8e1c
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-11 12:01:43 +0200

    Add toBeFalsy() and error handling

--- src/assertions.mjs
+++ src/assertions.mjs
@@ -1,5 +1,5 @@
 import { AssertionError } from './assertionError.mjs'
-import { applyColor } from './transform.mjs'
+import { RunnerError } from './runnerError.mjs'
 
 export const toBeDefined = (actual) => {
   if (actual === undefined) {
@@ -10,10 +10,27 @@ export const toBeDefined = (actual) => {
 }
 
 export const toHaveLength = (actual, expected) => {
+  if (!Array.isArray(actual)) {
+    throw new RunnerError(`Expected ${actual} is not an array`)
+  }
+  if (typeof expected !== 'number') {
+    throw new RunnerError(`${expected} is not a number`)
+  }
   if (actual.length !== expected) {
     throw new AssertionError(
       'value to have length <expected> but it was <actual>',
       { actual: actual.length, expected }
     )
+  }
+}
+
+export const toBeFalsy = (actual) => {
+  if (typeof actual !== 'boolean') {
+    throw new RunnerError(`${actual} is not an expected boolean value`)
+  }
+  if (actual !== false) {
+    throw new AssertionError('<actual> to be false', {
+      actual,
+    })
   }
 }
